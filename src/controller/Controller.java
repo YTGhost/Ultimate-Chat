00f5970bcbc2705ef59d16ddb89af14ae66d4e85
YTGhost <283304489@qq.com>
2019-12-11 13:43:27 +0800

    modify name

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,4 +1,4 @@
-package Controller;
+package controller;
 
 /**
  * @author 邓梁
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,4 @@
-package Main;
+package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
